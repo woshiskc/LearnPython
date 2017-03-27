30d9c58633094b461012a85ae89a6f3e8c31aac1
woshiskc <289503308@qq.com>
2017-03-27 23:02:12 +0800

    Add files via upload

---
+++ Day6/ex7.py
@@ -0,0 +1,26 @@
+#-*- coding utf-8 -*-
+print "Marry had a little lamb." #print text
+print "Its fleece was white as %s." % 'snow' #print text including format variable. string can be send and replace the variable
+print "And everywhere that Marry went." # print text
+print "."* 10 #what do that do ? print '.' 10 times in the same line
+
+end1 = "C" # define end1
+end2 = "h" # define end2
+end3 = "e" # define end3
+end4 = "e" # define end4
+end5 = "s" # define end5
+end6 = "e" # define end6
+end7 = "B" # define end7
+end8 = "u" # define end8
+end9 = "r" # define end9
+end10 = "g" # define end10
+end11 = "e" # define end11
+end12 = "r" # define end12
+
+# watch that comma at the end try removing it to see what happen
+print end1 + end2 + end3 + end4 + end5 + end6,
+print end7 + end8 + end9 + end10 + end11 + end12
+
+# without the comma, the results of these 2 print() are in different lines 
+print end1 + end2 + end3 + end4 + end5 + end6
+print end7 + end8 + end9 + end10 + end11 + end12
