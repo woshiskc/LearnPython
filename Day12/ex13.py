93ba38aaad622cdf6dc905902b2d72ab0347205b
woshiskc <289503308@qq.com>
2017-04-01 22:04:40 +0800

    Add files via upload

---
+++ Day12/ex13.py
@@ -0,0 +1,16 @@
+# -*- coding utf-8 -*-
+from sys import argv
+
+script,first,second,third = argv
+
+script = raw_input("input script name")
+first = raw_input("input 1st variable's name")
+second = raw_input("input 2nd variable's name")
+third = raw_input("input 3rd variable's name")
+
+
+
+print "The script is called:",script
+print "Your first variable is:",first
+print "Your second variable is:",second
+print "Your third variable is:",third
